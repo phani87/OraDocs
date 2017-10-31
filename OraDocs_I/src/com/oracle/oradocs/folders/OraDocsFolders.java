10745aff18bd0ee170abe5376fee62f4e234549c
phani87 <phanivasudev@gmail.com>
2017-10-31 11:43:06 -0400

    Updating OraDocsFolder.java

--- OraDocs_I/src/com/oracle/oradocs/folders/OraDocsFolders.java
+++ OraDocs_I/src/com/oracle/oradocs/folders/OraDocsFolders.java
@@ -25,16 +25,105 @@ import jdk.nashorn.internal.parser.JSONParser;
 
 public class OraDocsFolders {
 
-	private final String URL = "https://oradocs-corp.documents.us2.oraclecloud.com/documents/api/1.2/folders/F30CFB1F2E8D65077F6A5B0BF6C3FF17C1177A968060/items";
+	private final static String URL = "https://oradocs-corp.documents.us2.oraclecloud.com/documents/api/1.2/folders/";
+	private final static String MASTER_FOLDER = "F30CFB1F2E8D65077F6A5B0BF6C3FF17C1177A968060/";
 	private final String USER_AGENT = "Mozilla/5.0";
+	
+	
+	private static FolderVO folderVo;
 
-	public void getFolders() throws ClientProtocolException, IOException {
+	
+	
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		new OraDocsFolders().flowOrganizer();
+	}
+	
+	private void flowOrganizer() {
+		List<FolderVO> MASTER_FOLDERS = null;
+		String json_response = "";
+		try {
+			json_response = OraDocsFolders.getFolders(URL + MASTER_FOLDER + "items");
+			MASTER_FOLDERS = new OraDocsFolders().oraDocsJsonParser(json_response);
+			iterateFolderList(MASTER_FOLDERS);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	private void iterateFolderList(List<FolderVO> folderMap) {
+		for(FolderVO folderID : folderMap) {
+			try {
+				String temp_json = getFolders(URL+folderID.getCurr_folder()+"/items");
+				List<FolderVO> subFolderList = oraDocsJsonParser(temp_json);
+				iterateFolderList(subFolderList);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//helper methods
+	
+	private List<FolderVO> oraDocsJsonParser(String data) {
+			List<FolderVO>  folderMap = new ArrayList<FolderVO>();
+		    JSONObject jsonObject = new JSONObject(data);
+		    FolderVO folderVo = null;
+		    int size = Integer.parseInt(jsonObject.get("count").toString());
+		    if(size>0) {
+		    	JSONArray array = (JSONArray) jsonObject.get("items"); // it should be any array name
+			    List<FileVO> fileMap = new ArrayList<>();
+			    Iterator<Object> iterator = array.iterator();
+			    
+			    while (iterator.hasNext())
+			    {
+			    	//iterator.next();
+			        Object it = iterator.next();
+			        JSONObject folders = (JSONObject) it;
+			        if(folders.get("type").equals("folder")) {
+			        	folderVo = new FolderVO();
+			        	folderVo.setCurr_folder(folders.get("id").toString());
+			        	folderVo.setPrev_folder(folders.get("parentID").toString());
+			        	folderVo.setFolder_name(folders.get("name").toString());
+			        	folderVo.setTraversed(true);
+			        	folderVo.setFilesUpdated(false);
+			        	//System.out.println("Folder>>>" +folderVo.getPrev_folder()+"/"+folderVo.getCurr_folder());
+			        	folderMap.add(folderVo);
+			        	
+			        }else if(folders.get("type").equals("file")) {
+			        	FileVO fileVo = new FileVO();
+			        	fileVo.setFile_id(folders.get("id").toString());
+			        	fileVo.setFolder_id(folders.get("parentID").toString());
+			        	fileVo.setFile_name(folders.get("name").toString());
+			        	//fileVo.setPrev_folder_id(folders.get("parentID").toString());
+			        	fileVo.setFileUpdated(true);
+			        	fileMap.add(fileVo);
+			        }
+			        
+			    }
+			    if(fileMap.size()>0) {
+			    	updateFiles(fileMap);
+			    }
+		    }
+		    return folderMap;
+		    
+	}
+	
+	private void updateFiles(List<FileVO> files) {
+		for(FileVO file : files) {
+			System.out.println("Updating..."+file.getFile_name());
+			FolderVO folderVo = new FolderVO();
+			//folderVo.setCurr_folder(curr_folder);
+		}
+	}
+	private static String getFolders(String URL) throws ClientProtocolException, IOException {
 
 		HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet(URL);
 
 		// add request header
-		request.addHeader("authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsIng1dCI6InUwY1BSU3dCYkE3V1BkZGR1MGo2QlBfRXZxVSIsImtpZCI6ImNvcnAuY2VydCJ9.eyJzdWIiOiJzaGlraGFyLm1pc2hyYUBvcmFjbGUuY29tIiwib3JhY2xlLm9hdXRoLnVzZXJfb3JpZ2luX2lkX3R5cGUiOiJMREFQX1VJRCIsIm9yYWNsZS5vYXV0aC51c2VyX29yaWdpbl9pZCI6InNoaWtoYXIubWlzaHJhQG9yYWNsZS5jb20iLCJpc3MiOiJjb3JwIiwib3JhY2xlLm9hdXRoLnN2Y19wX24iOiJjb3JwU2VydmljZVByb2ZpbGUiLCJpYXQiOjE1MDg3NzM3MjcsIm9yYWNsZS5vYXV0aC5wcm4uaWRfdHlwZSI6IkxEQVBfVUlEIiwib3JhY2xlLm9hdXRoLnRrX2NvbnRleHQiOiJyZXNvdXJjZV9hY2Nlc3NfdGsiLCJleHAiOjE1MDkzNzg1MjYsImF1ZCI6WyJodHRwczovL29yYWRvY3MtY29ycC5kb2N1bWVudHMudXMyLm9yYWNsZWNsb3VkLmNvbTo0NDMvZG9jdW1lbnRzIl0sIm9yYWNsZS5vYXV0aC5zdWIuaWRfdHlwZSI6IkxEQVBfVUlEIiwicHJuIjoic2hpa2hhci5taXNocmFAb3JhY2xlLmNvbSIsImp0aSI6ImU3ZDdiZGQxLTQ1YjQtNDI5Yi04Mjg4LTU0NzgyZTYxZDUyNSIsIm9yYWNsZS5vYXV0aC5jbGllbnRfb3JpZ2luX2lkIjoiYmYyMDYxNTMtNWE0ZS00MjEyLTk5NTYtMjVhNmM0NDRlNDAyIiwib3JhY2xlLm9hdXRoLnNjb3BlIjoiaHR0cHM6Ly9vcmFkb2NzLWNvcnAuZG9jdW1lbnRzLnVzMi5vcmFjbGVjbG91ZC5jb206NDQzL2RvY3VtZW50cyIsInVzZXIudGVuYW50Lm5hbWUiOiJjb3JwIiwib3JhY2xlLm9hdXRoLmlkX2RfaWQiOiI2NjY4ODgxNzI5ODEyNTI1NCJ9.KMPTpmcxabZ9D-SUVGWgTSMGaA-eTDZCNb_CVXr7suFyFbWcHlK89oNrBpsdu1-9jWAwh7nA5wUjXor9Bgco0WYWZKSYUBA3JQ5RknK6j8JpU4auwDb-u7q3k4dG9zLzp7ipPxmNqRhGLw2m_1DH7ZmrdkHoXXalecj6w3y6oRJ833UnLlWOpElXkY9d2hSHcaVqlX-a6-eqqJrZtYk74uwXqcajBEyLiWgNOr6Xg5m4hck8OVXzS0IhsfiNAuBcjxJqluStBGbseROMOgkiXbPv0jCPhHr-X3s96PXOf5CKrWtXCHe3eUjoov99yf7E5RaZ0q9xm57iz8zSDtErKw");
+		request.addHeader("authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsIng1dCI6InUwY1BSU3dCYkE3V1BkZGR1MGo2QlBfRXZxVSIsImtpZCI6ImNvcnAuY2VydCJ9.eyJzdWIiOiJzaGlraGFyLm1pc2hyYUBvcmFjbGUuY29tIiwib3JhY2xlLm9hdXRoLnVzZXJfb3JpZ2luX2lkX3R5cGUiOiJMREFQX1VJRCIsIm9yYWNsZS5vYXV0aC51c2VyX29yaWdpbl9pZCI6InNoaWtoYXIubWlzaHJhQG9yYWNsZS5jb20iLCJpc3MiOiJjb3JwIiwib3JhY2xlLm9hdXRoLnN2Y19wX24iOiJjb3JwU2VydmljZVByb2ZpbGUiLCJpYXQiOjE1MDkzOTU0MzAsIm9yYWNsZS5vYXV0aC5wcm4uaWRfdHlwZSI6IkxEQVBfVUlEIiwib3JhY2xlLm9hdXRoLnRrX2NvbnRleHQiOiJyZXNvdXJjZV9hY2Nlc3NfdGsiLCJleHAiOjE1MTAwMDAyMjksImF1ZCI6WyJodHRwczovL29yYWRvY3MtY29ycC5kb2N1bWVudHMudXMyLm9yYWNsZWNsb3VkLmNvbTo0NDMvZG9jdW1lbnRzIl0sIm9yYWNsZS5vYXV0aC5zdWIuaWRfdHlwZSI6IkxEQVBfVUlEIiwicHJuIjoic2hpa2hhci5taXNocmFAb3JhY2xlLmNvbSIsImp0aSI6ImQ4NGRmNTA1LWM1ZWYtNDA5Yy05YTcxLTc0MGRlMzk3ODlmOSIsIm9yYWNsZS5vYXV0aC5jbGllbnRfb3JpZ2luX2lkIjoiYmYyMDYxNTMtNWE0ZS00MjEyLTk5NTYtMjVhNmM0NDRlNDAyIiwib3JhY2xlLm9hdXRoLnNjb3BlIjoiaHR0cHM6Ly9vcmFkb2NzLWNvcnAuZG9jdW1lbnRzLnVzMi5vcmFjbGVjbG91ZC5jb206NDQzL2RvY3VtZW50cyIsInVzZXIudGVuYW50Lm5hbWUiOiJjb3JwIiwib3JhY2xlLm9hdXRoLmlkX2RfaWQiOiI2NjY4ODgxNzI5ODEyNTI1NCJ9.oUNxqzNsdnx2DtHwehF62_6mcNQE4JYRanS-RHfAR0Ao-gNwS93DpXDDbn15KqvM91CTv_Hi6k-hFf7S9k7KjiW54rxVcFrJwG2aLsxA0qZ7_tG2DSu7jhPYXYg7Ke8qrk1mlwzTmXgGDHjXDloqXJH_GwNiOm2VbrCODb40VsC_rfJNAyMJ0_7lXem95af2kSER3ijJ5ylnwein94SSMtPQbtlzPiqbwUgLPKm9xMqQUYb7vWqOyeVz9c5krRI8srbWHh7_vQtwFJuho8KbM0_fVIcy3mFIu5ohF08Vr97vze8oCJZxj5BhI3HrA0Y1HoeAtbZ-CKTFakMTERX-0Q");
 		request.addHeader("cache-control", "no-cache");
 		
 		HttpResponse response = client.execute(request);
@@ -49,40 +138,9 @@ public class OraDocsFolders {
 		while ((line = rd.readLine()) != null) {
 			result.append(line);
 		}
-		oraDocsJsonParser(result.toString());
-		//JSONObject jsonRespose = new JSONObject(result.toString());
+		 return  result.toString();
 		
 		
-	}
-	
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		OraDocsFolders ord = new OraDocsFolders();
-		ord.getFolders();
-	}
-	
-	private void oraDocsJsonParser(String data) {
-			/*JSONParser parser = new JSONParser();
-		    Object obj = parser.parse(new String(data));*/
-		    Map<Object, Object> shareList = new HashMap<Object, Object>();
-		    JSONObject jsonObject = new JSONObject(data);
-		    JSONArray array = (JSONArray) jsonObject.get("items"); // it should be any array name
-
-		    Iterator<Object> iterator = array.iterator();
-		    while (iterator.hasNext())
-		    {
-		        Object it = iterator.next();
-		        JSONObject folders = (JSONObject) it;
-		        if(folders.get("type").equals("folder")) {
-		        	shareList.put(folders.get("id"), folders.get("name"));
-		        }
-		        
-		    }
-		    Iterator it = shareList.entrySet().iterator();
-		    while (it.hasNext())
-		    {
-		        Map.Entry value = (Map.Entry) it.next();
-		        System.out.println("Folder id: " + value.getKey() + " and name: " + value.getValue());
-		    }
 	}
 
 }
